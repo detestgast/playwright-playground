aeb6fc9c23953fc4024e3547a11da632fdaae4e6
Martin de Gast <martin.degast@benerail.com>
2026-06-23 17:56:20 +0200

    chore: polish test scenario

--- tests/insurance-premium-calculation-e2e.spec.ts
+++ tests/insurance-premium-calculation-e2e.spec.ts
@@ -1,28 +1,29 @@
 import { test, expect } from '../src/fixtures/fixtures';
 import * as allure from 'allure-js-commons';
 import { adultFemale, adultMale } from './scenarios/adult-happy-flow';
+import { formatDate } from '../src/utils/date';
 
-test.describe('DELA uitvaartverzekering - Happy flow', () => {
+test.describe('DELA uitvaartverzekering', () => {
   const testScenarios = [{ scenario: adultMale }, { scenario: adultFemale }];
 
   for (const { scenario } of testScenarios) {
-    test.describe(`${scenario.gender} adult person "${scenario.firstName} ${scenario.lastName}" choosing ${scenario.insuranceOptions.insuranceType}`, () => {
+    test.describe(`${scenario.gender} adult person ${scenario.firstName} ${scenario.lastName} choosing ${scenario.insuranceOptions.insuranceType}`, () => {
       test('should perform a premium calculation', async ({ uitvaartverzekeringAfsluitenPage }) => {
-        await allure.step('Visit the insurance premium calculation page', async () => {
+        await allure.step('Visit insurance premium calculation page', async () => {
           await uitvaartverzekeringAfsluitenPage.visit();
           await expect(uitvaartverzekeringAfsluitenPage.mainContent).toMatchAriaSnapshot({
             name: 'insurance-premium-calculation-step1-start.aria.yml',
           });
         });
 
-        await allure.step('Enter date of birth and continue', async () => {
+        await allure.step(`Enter date of birth ${formatDate(scenario.dateOfBirth)} and continue`, async () => {
           await uitvaartverzekeringAfsluitenPage.enterGeboortedatumAndContinue(scenario.dateOfBirth);
           await expect(uitvaartverzekeringAfsluitenPage.mainContent).toMatchAriaSnapshot({
             name: 'insurance-premium-calculation-step1-choose-insurance.aria.yml',
           });
         });
 
-        await allure.step('Select insurance options', async () => {
+        await allure.step(`Select insurance type ${scenario.insuranceOptions.insuranceType}`, async () => {
           await uitvaartverzekeringAfsluitenPage.selectInsuranceOption(scenario.insuranceOptions.insuranceType);
           await uitvaartverzekeringAfsluitenPage.selectAmountOrAdditionalAmount(scenario);
         });
