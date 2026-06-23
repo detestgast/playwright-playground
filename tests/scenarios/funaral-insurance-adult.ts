680caaf23acdf0afa2ed96ecd2b414428f20da57
Martin de Gast <martin.degast@benerail.com>
2026-06-23 20:54:07 +0200

    chore: rename funaral insurance spec

--- tests/scenarios/funaral-insurance-adult.ts
+++ tests/scenarios/funaral-insurance-adult.ts
@@ -1,4 +1,5 @@
-import { INSURANCE_OPTIONS } from '../../src/consts/insurance-options';
+import { DURATION } from '../../src/consts/durations';
+import { INSURANCE_TYPE } from '../../src/consts/insurance_types';
 import { InsuranceOptions } from '../../src/types/insuranceOptions';
 import { Persona } from '../../src/types/persona';
 
@@ -8,8 +9,9 @@ export const adultMale = {
   dateOfBirth: new Date('1976-11-16'),
   gender: 'male',
   insuranceOptions: {
-    insuranceType: INSURANCE_OPTIONS.MONEY,
+    insuranceType: INSURANCE_TYPE.MONEY,
     insuredAmount: 15000,
+    duration: DURATION.UNTIL_AGE_65,
   },
 } satisfies Persona & InsuranceOptions;
 
@@ -19,7 +21,8 @@ export const adultFemale = {
   dateOfBirth: new Date('1988-09-13'),
   gender: 'female',
   insuranceOptions: {
-    insuranceType: INSURANCE_OPTIONS.SERVICE,
+    insuranceType: INSURANCE_TYPE.SERVICE,
     additionalAmount: 2000,
+    duration: DURATION.UNTIL_DEATH,
   },
 } satisfies Persona & InsuranceOptions;
