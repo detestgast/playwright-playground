e444a499fd4554ef7b303ed3e0c1f5aa679a45f7
Martin de Gast <martin.degast@benerail.com>
2026-06-24 20:22:26 +0200

    chore: remove manually attaching the trace as it comes from trace:on anyway

--- src/fixtures/fixtures.ts
+++ src/fixtures/fixtures.ts
@@ -1,6 +1,4 @@
 import { test as base } from '@playwright/test';
-import * as allure from 'allure-js-commons';
-import fs from 'fs';
 import { UitvaartverzekeringAfsluitenPage } from '../pages/uitvaartverzekering-afsluiten-page';
 
 type DelaFixtures = {
@@ -12,14 +10,6 @@ export const test = base.extend<DelaFixtures>({
     const uitvaartverzekeringAfsluitenPage = new UitvaartverzekeringAfsluitenPage(page);
     await use(uitvaartverzekeringAfsluitenPage);
   },
-});
-
-test.afterEach(async ({}, testInfo) => {
-  const tracePath = testInfo.outputPath('trace.zip');
-
-  if (fs.existsSync(tracePath)) {
-    allure.attachment('Playwright Trace', fs.readFileSync(tracePath), 'application/zip');
-  }
 });
 
 export { expect } from '@playwright/test';
